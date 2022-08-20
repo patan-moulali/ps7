bd02718eb6516914a160e1514d1fdcb72d570f87
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-20 15:06:32 +0530

    Add project files.

---
+++ ps7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Enter a digit: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            // Part A: "num num num num" using Write
+            Console.Write(num);
+            Console.Write(" ");
+            Console.Write(num);
+            Console.Write(" ");
+            Console.Write(num);
+            Console.Write(" ");
+            Console.Write(num);
+            Console.WriteLine();
+
+            // Part B: "numnumnumnum" using Write
+            Console.Write(num);
+            Console.Write(num);
+            Console.Write(num);
+            Console.WriteLine(num);
+            Console.WriteLine();
+
+            // Part C: "num num num num" using {0}
+            Console.WriteLine("{0} {0} {0} {0}", num);
+
+            // Part D: "numnumnumnum" using {0}
+            Console.WriteLine("{0}{0}{0}{0}", num);
+            Console.Read();
+        }
+    }
+}
